7de139156a9f3d8a823d9ffc22a25fba44f38193
LaichaMK <laichamouhamankoura@gmail.com>
2022-08-13 16:43:58 +0200

    creation de la page repas avec l'API fectch

--- pages/fetch.js
+++ pages/fetch.js
@@ -1,65 +1,39 @@
-/** @format */
+fetch("http://localhost:3004/products").then(response =>{
+  return response.json(); 
+}).then(data =>{
+  console.log (data);
+  data.forEach(element => {
+    // cretion des element de la div(img1)
+    let div = document.createElement("div");
+    div.setAttribute("class", "img1");
+    let img =document.createElement("img");
+    img.setAttribute("height" , "150px");
+    img.setAttribute("width", "150px");
+    let h2 = document.createElement("h2");
+    let  div2=document.createElement("div") 
+    div2.setAttribute("class", "prix")
+    let span= document.createElement("span");
+    let button= document.createElement("button")
+    button.style.marginBottom ="20px";
 
-(function () {
-    fetch('http://localhost:3000/products')
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        // Manipulation du DOM
-        const product_list = document.getElementById('container1');
-  
-        data.forEach((product) => {
-          // product item
-          let product_item = document.createElement('img1');
-          product_item.classList.add('product_item');
-  
-          let product_img = document.createElement('div');
-          product_img.classList.add('product_img');
-  
-          let img = document.createElement('img');
-          img.classList.add('w-50');
-  
-          let product_content = document.createElement('div');
-          product_content.classList.add('product_content');
-  
-          let h5 = document.createElement('h5');
-          let link = document.createElement('a');
-  
-          let product_details = document.createElement('div');
-          product_details.classList.add('product_details');
-  
-          let span = document.createElement('span');
-          span.classList.add('product_price');
-  
-          let btn_cart = document.createElement('button');
-          btn_cart.classList.add('addToCard_btn');
-          btn_cart.innerHTML = 'ADD TO CART';
-  
-          // image
-          img.src = `./images/${product.img}`;
-          img.alt = product.title;
-          // titre du produit
-          link.innerHTML = product.title;
-          // product price
-          span.innerHTML = product.price;
-          span.innerHTML = `${product.price} FCFA`;
-  
-          product_details.appendChild(span);
-          product_details.appendChild(btn_cart);
-  
-          h5.appendChild(link);
-  
-          product_content.appendChild(h5);
-          product_content.appendChild(product_details);
-  
-          product_img.appendChild(img);
-  
-          product_item.appendChild(product_img);
-          product_item.appendChild(product_content);
-  
-          product_list.appendChild(product_item);
-        });
-      });
-  })();
-  
+// relier les elemnts du tableau d'object du fetch aux elements creer
+    img.src=element.img;
+    h2.innerHTML=element.title;
+    span.innerHTML=element.price;
+    button.innerHTML= "add to cart";
+
+// ajout d'un enfant sur un element
+    div.appendChild(img);
+    div.appendChild(h2);
+    div2.appendChild(span);
+    div2.appendChild(button);
+    div.appendChild(div2);
+    
+  // renvoyer la div(img1) dans la div(container1)
+    document.getElementById('container1').appendChild(div);
+
+
+  });
+}).catch(error=>{
+  console.error(error);
+})
